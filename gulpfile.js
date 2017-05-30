9206bf5d2d338f8d90b917f8e07fd3a296e28c15
Man Wu <kpwqm@163.com>
2017-05-31 00:32:41 +0800

    add gulp -> css-spriter
    add gulp -> gulp-css-spriter

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var spriter = require('gulp-css-spriter');
+
+gulp.task("css", function(){
+  var timestamp = +new Date();
+
+  return gulp.src("./static/css/*.css")
+    .pipe(spriter({
+      'spriteSheet': './dist/images/sprite' + timestamp + '.png',
+      'pathToSpriteSheetFromCSS': '../images/sprite' + timestamp + '.png'
+    }))
+    .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('css-watch',function(cb){
+  var watcher = gulp.watch('./static/css/*.*', ['css']);
+  watcher.on('change', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
+});
