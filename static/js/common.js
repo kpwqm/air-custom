5df7bee76c2f133f61aa3daa81101ade426bdcb8
Man Wu <kpwqm@163.com>
2017-06-07 00:43:08 +0800

    updated page > news, cart

--- static/js/common.js
+++ static/js/common.js
@@ -1,16 +1,19 @@
 console.log('loaded common.js ...');
-jq(document).ready(function(){
-  // 右侧置顶
-  jq('#fixed-top').on("click", function(){
-    jq('html, body').animate({scrollTop:0}, 200);
-  })
 
-  jq('.user-name__a, .user-nav-wrap').on("mouseenter", function(){
-    jq('.user-nav-wrap').show();
-  })
-  .on("mouseleave", function(){
-    jq('.user-nav-wrap').hide();
-  })
+// 滚动右侧固定栏显示
+jq(window).scroll(function(){
+  jq(document).scrollTop() > 10 ?
+    jq(".fixed-bar-box").fadeIn() : jq(".fixed-bar-box").fadeOut();
+})
+// 右侧置顶
+jq('#fixed-top').on("click", function(){
+  jq('html, body').animate({scrollTop:0}, 200);
+})
+
+jq('.user-name__a, .user-nav-wrap').on("mouseenter", function(){
+  jq('.user-nav-wrap').show();
+}).on("mouseleave", function(){
+  jq('.user-nav-wrap').hide();
 })
   
 console.log('End common.js ...');
